c2154850f036ab3ed8d8ed9dc1d83b3b887ccd18
code-che <codeche@gmail.com>
2021-01-31 18:09:03 +0330

    level: add node

--- src/components/CreateDiagram.js
+++ src/components/CreateDiagram.js
@@ -5,7 +5,7 @@ import 'beautiful-react-diagrams/styles.css';
 
 
 const NewNodeRender = ({ id, content, data, inputs }) => (
-    <div className="new-node">
+    <div className="new-node" onMouseUp={(event)=>data.onMouseUp(event)}>
         <div className="header-node">
             <span className="controller-name">ali</span>
             <button onClick={()=>data.onClick(id)} >X</button>
@@ -19,8 +19,8 @@ const NewNodeRender = ({ id, content, data, inputs }) => (
     </div>
 );
 
-const NodeRender = ({ content, inputs }) => (
-    <div className="node">
+const NodeRender = ({ id, content, data, inputs }) => (
+    <div className="node" onMouseUp={(event)=>data.onMouseUp(event, id)}>
         <div className="header-node">
             <span className="controller-name">ali</span>
         </div>
@@ -39,11 +39,90 @@ function CreateDiagram( props ) {
     let nodesOfTree = [];
     let links = [];
 
+    const updateCoordinates = ( event, id ) => {
+        console.log("che khabar");
+        console.log(event);
+        console.log(id);
+        let x = 0;
+        let y = 0;
+        for ( let node of schema.nodes ){
+            if ( node.id === id ) {
+                x = node.coordinates[0] - node.coordinates[0]%200;
+                y = node.coordinates[1] - node.coordinates[1]%100;
+            }
+        }
+
+        const axios = require('axios');
+        axios.post(`https://project.dinavision.org/api/v1/Project/UpdateProjectLocation?projectId=${id}&xLocation=${x}&yLocation=${y}`)
+            .then(response => {
+                console.log(response.message)
+            }).catch( error => {
+                console.log(error)
+            })
+    }
+
+    const [newNodeListState, setNewNodeList] = useState([]);
+    // const [hasLink]
+    const updateNodeData = ( event, id ) => {
+        let x = 0;
+        let y = 0;
+        for ( let node of schema.nodes ){
+            if ( node.id === id ) {
+                x = node.coordinates[0] - node.coordinates[0]%200;
+                y = node.coordinates[1] - node.coordinates[1]%100;
+            }
+        }
+        let parentId = "";
+        for ( let link of schema.links ){
+            if ( id === link["input"] ){
+                if (link["output"] < link["input"]) {
+                    parentId = link["output"];
+                }
+            }
+            if ( id === link["output"] ){
+                if (link["input"] < link["output"]) {
+                    parentId = link["input"];
+                }
+            }
+        }
+        let temp;
+        let data;
+        if (parentId !== ''){
+            temp = nodes.filter(node => node.id === parentId );
+            data = {
+                id: id,
+                parentId: parentId,
+                route: temp[0].route + `/${parentId}`,
+                xLocation: x,
+                yLocation: y
+            }
+
+        }
+
+
+
+        // setNewNodeList(prevState =>{
+        //     for ( let node of prevState ){
+        //         if ( node.id === id )
+        //
+        //     }
+        //     return [...prevState, data]
+        // })
+
+        const axios = require('axios');
+        axios.post(`https://project.dinavision.org/api/v1/Project/UpdateProjectLocation?projectId=${id}&xLocation=${x}&yLocation=${y}`)
+            .then(response => {
+                console.log(response.message)
+            }).catch( error => {
+            console.log(error)
+        })
+    }
+
     const createNode = (node) => {
         let y = 0;
         let x = 0;
-        if (node["coordinates"] === undefined) {
-            y = node.treeLevel * 200;
+        if (node.coordinates.length === 0 ) {
+            y = node.treeLevel * 300;
             x = Math.floor(Math.random() * Math.floor(1000));
         }
         else {
@@ -55,6 +134,7 @@ function CreateDiagram( props ) {
             content: node.title,
             coordinates: [x, y],
             render: NodeRender,
+            data: {onMouseUp: updateCoordinates},
             inputs: [ { id: node.id } ],
         }
     }
@@ -93,50 +173,80 @@ function CreateDiagram( props ) {
         removeNode(nodeToRemove);
     };
 
+
+    const [idRenderState, setIdRender] = useState(999999);
     const addNewNode = () => {
+        setIdRender(prevState => prevState + 1)
+        let newNodeId = idRenderState.toString();
+        setNewNodeList(prevState => [...prevState, newNodeId]);
         const nextNode = {
-            id: `node-${schema.nodes.length+1}`,
+            id: newNodeId,
             content: titleInputState,
             coordinates: [
                 schema.nodes[schema.nodes.length - 1].coordinates[0] + 100,
                 schema.nodes[schema.nodes.length - 1].coordinates[1],
             ],
-            render: NewNodeRender,
-            data: {onClick: deleteNodeFromSchema},
-            inputs: [{ id: `port-${Math.random()}`}]
+            render: updateNodeData,
+            data: {
+                onClick: deleteNodeFromSchema,
+                onMouseUp: updateCoordinates
+            },
+            inputs: [{ id: newNodeId}]
         };
 
         addNode(nextNode);
     }
 
     const submit = (event) => {
-        let listOfNodes = []
-        for ( let node of schema.nodes) {
-            let x = node.coordinates[0] - node.coordinates[0]%100;
-            let y = node.coordinates[1] - node.coordinates[1]%200;
-            let parentId = "";
-            let inputId = node.inputs[0]["id"];
-            for ( let link of schema.links ){
-                if ( inputId === link["input"]){
-                    if (link["output"] < link["input"]) {
-                        parentId = link["output"];
-                    }
-                }
-                if ( inputId === link["output"]){
-                    if (link["input"] < link["output"]) {
-                        parentId = link["input"];
-                    }
-                }
-            }
-            let temp = {
-                title: node.content,
-                id: node.id,
-                treeLevel: y/200,
-                coordinates: [x, y],
-                parentId: parentId
-            }
-            listOfNodes.push(temp);
-        }
+        // let listOfNodes = []
+        // for ( let newNode of newNodeListState) {
+        //     let parentId = "";
+        //     for ( let link of schema.links ){
+        //         if ( newNode === link["input"] ){
+        //             if (link["output"] < link["input"]) {
+        //                 parentId = link["output"];
+        //             }
+        //         }
+        //         if ( newNode === link["output"] ){
+        //             if (link["input"] < link["output"]) {
+        //                 parentId = link["input"];
+        //             }
+        //         }
+        //     }
+        //     let temp = nodes.filter(node => node.id === parentId );
+        //     let data = {
+        //         id: newNode,
+        //         parentId: parentId,
+        //         route: temp[0].route + `/${parentId}`,
+        //     }
+        // }
+        // for ( let node of schema.nodes ) {
+        //     let x = node.coordinates[0] - node.coordinates[0]%100;
+        //     let y = node.coordinates[1] - node.coordinates[1]%200;
+        //     let parentId = "";
+        //     let inputId = node.inputs[0]["id"];
+        //     // for ( let link of schema.links ){
+        //     //     if ( inputId === link["input"]){
+        //     //         if (link["output"] < link["input"]) {
+        //     //             parentId = link["output"];
+        //     //         }
+        //     //     }
+        //     //     if ( inputId === link["output"]){
+        //     //         if (link["input"] < link["output"]) {
+        //     //             parentId = link["input"];
+        //     //         }
+        //     //     }
+        //     // }
+        //     let temp = {
+        //         title: node.content,
+        //         id: node.id,
+        //         treeLevel: y/200,
+        //         coordinates: [x, y],
+        //         parentId: parentId
+        //     }
+        //     listOfNodes.push(temp);
+        // }
+        console.log(newNodeListState);
     }
 
     const initialSchema = createSchema({
--- src/components/Diagram.js
+++ src/components/Diagram.js
@@ -66,38 +66,117 @@ function Diagram() {
         parentId : ""
     }
 
-    let listOfNodes = [];
     // listOfNodes = [node1, node2, node3, node4, node5, node6, node7, node8, node9, node21]
 
-    const [dataState, setDataState] = useState([])
+    const [dataState, setDataState] = useState({
+        projects: [],
+        routes: [],
+        nodes: []
+    });
+    const [dataIsReady, setDataIsReady] = useState(false);
+    const [listOfNodesState, setListOfNodesState] = useState([]);
+    const [styleState, setStyleState] = useState({
+        height: "1000px",
+        width : "1500px",
+    });
 
-    const axios = require('axios');
-    axios.get('https://project.dinavision.org/api/v1/Project/GenerateLevels?userId=1233').then(response => {
-        setDataState(response.data.data.projects)
-    }).catch( error => {
-        console.log(error);
-    })
-
-
-    const click = (event) => {
-        console.log(dataState);
-
-        for ( let project of dataState ){
-            // console.log(typeof project.id);
-            listOfNodes.push({
-                title: project.title,
-                id: project.id.toString(),
-                treeLevel: 3,
-                parentId : project.parentId.toString()
+    useEffect(() => {
+        const axios = require('axios');
+        axios.get('https://project.dinavision.org/api/v1/Project/GenerateLevels?userId=1233').then(response => {
+            setDataState({
+                projects: response.data.data.projects,
+                routes:response.data.data.projects.filter( item => item.parentId === 0),
+                nodes: []
             })
+        }).catch( error => {
+            console.log(error);
+        })
+    }, [])
+
+
+    let listOfNodes = [];
+    const render = (event) => {
+        console.log(dataState.projects);
+
+        let maxWidth = 0, maxHeight = 0;
+        let routeProject = dataState.routes[2];
+        let coordinatesRoute = [];
+        if (!(routeProject.xLocation === 0 && routeProject.yLocation === 0)) {
+            coordinatesRoute = [routeProject.xLocation, routeProject.yLocation];
+            if ( maxWidth < routeProject.xLocation)
+                maxWidth = routeProject.xLocation;
+            if ( maxHeight < routeProject.yLocation)
+                maxHeight = routeProject.yLocation;
         }
+        listOfNodes.push({
+            title: routeProject.title,
+            id: routeProject.id.toString(),
+            treeLevel: routeProject.treeLevel,
+            parentId: '',
+            coordinates: coordinatesRoute
+        })
+        let routeId = routeProject.id.toString();
+
+        for ( let project of dataState.projects ){
+            // console.log(project.route);
+            if ( project.route !== null) {
+                let parentList = project.route.split("/");
+                if ( parentList.indexOf(routeId) >= 0) {
+                    let coordinates = [];
+                    if (!(project.xLocation === 0 && project.yLocation === 0)) {
+                        coordinates = [project.xLocation, project.yLocation];
+                        if ( maxWidth < project.xLocation)
+                            maxWidth = project.xLocation;
+                        if ( maxHeight < project.yLocation)
+                            maxHeight = project.yLocation;
+                    }
+                    listOfNodes.push({
+                        title: project.title,
+                        id: project.id.toString(),
+                        treeLevel: project.treeLevel,
+                        parentId: project.parentId.toString(),
+                        coordinates: coordinates,
+                        route: project.route
+                    })
+                }
+            }
+        }
+        if ( 1000 < maxHeight * 2 ){
+            setStyleState( prevState => ({
+                ...prevState,
+                height: (maxHeight * 2).toString() + "px",
+            }))
+        }
+        if ( 1500 < maxWidth * 2 ){
+            setStyleState( prevState => ({
+                ...prevState,
+                width: (Math.floor(maxWidth * 1.5)).toString() + "px",
+            }))
+        }
+        setListOfNodesState(listOfNodes);
+        setDataIsReady(true);
         console.log(listOfNodes);
+        // console.log(listOfNodesState);
+        // console.log(dataIsReady);
+    }
+
+    const creatDiagram = () => {
+        if (dataIsReady){
+            return (
+                <div className="frame" style={styleState}>
+                    <CreateDiagram nodes={listOfNodesState}/>
+                </div>
+            )
+        }
+        else
+            return null;
     }
 
     return (
         <>
-            <button onClick={click}>click</button>
-            <CreateDiagram nodes={listOfNodes}/>
+            <button onClick={render}>click</button>
+            {/*<CreateDiagram nodes={listOfNodes}/>*/}
+            {creatDiagram()}
         </>
     );
 }
